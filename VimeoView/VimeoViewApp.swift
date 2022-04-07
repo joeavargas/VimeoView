0c147eca95f5a6f91fa1f9000174623e337dd28c
Joe Vargas <joeavargas@outlook.com>
2022-04-07 10:09:30 -0500

    Import skeleton project

---
+++ VimeoView/VimeoViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  VimeoViewApp.swift
+//  VimeoView
+//
+//  Created by Joe Vargas on 4/7/22.
+//
+
+import SwiftUI
+
+@main
+struct VimeoViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
